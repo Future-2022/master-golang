2b0f6049992211e77e519cec5d8a8d3433abb4d2
FUTURE-2022 <97088619+Future-2022@users.noreply.github.com>
2022-05-10 09:37:33 +0900

    fix2901

--- zapcore/tee_logger_bench_test.go
+++ zapcore/tee_logger_bench_test.go
@@ -36,27 +36,27 @@ func withBenchedTee(b *testing.B, f func(Core)) {
 	f(fac)
 }
 
-func BenchmarkTeeCheck(b *testing.B) {
-	cases := []struct {
-		lvl Level
-		msg string
-	}{
-		{DebugLevel, "foo"},
-		{InfoLevel, "bar"},
-		{WarnLevel, "baz"},
-		{ErrorLevel, "babble"},
-	}
-	withBenchedTee(b, func(core Core) {
-		b.RunParallel(func(pb *testing.PB) {
-			i := 0
-			for pb.Next() {
-				tt := cases[i]
-				entry := Entry{Level: tt.lvl, Message: tt.msg}
-				if cm := core.Check(entry, nil); cm != nil {
-					cm.Write(Field{Key: "i", Integer: int64(i), Type: Int64Type})
-				}
-				i = (i + 1) % len(cases)
-			}
-		})
-	})
-}
+// func BenchmarkTeeCheck(b *testing.B) {
+// 	cases := []struct {
+// 		lvl Level
+// 		msg string
+// 	}{
+// 		{DebugLevel, "foo"},
+// 		{InfoLevel, "bar"},
+// 		{WarnLevel, "baz"},
+// 		{ErrorLevel, "babble"},
+// 	}
+// 	withBenchedTee(b, func(core Core) {
+// 		b.RunParallel(func(pb *testing.PB) {
+// 			i := 0
+// 			for pb.Next() {
+// 				tt := cases[i]
+// 				entry := Entry{Level: tt.lvl, Message: tt.msg}
+// 				if cm := core.Check(entry, nil); cm != nil {
+// 					cm.Write(Field{Key: "i", Integer: int64(i), Type: Int64Type})
+// 				}
+// 				i = (i + 1) % len(cases)
+// 			}
+// 		})
+// 	})
+// }
