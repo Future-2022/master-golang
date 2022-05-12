92ead042985f415714f969fac67bf99bef71d5d2
FUTURE-2022 <97088619+Future-2022@users.noreply.github.com>
2022-05-12 09:38:59 +0900

    fix892143

--- zapcore/buffered_write_syncer_bench_test.go
+++ zapcore/buffered_write_syncer_bench_test.go
@@ -29,25 +29,25 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func BenchmarkBufferedWriteSyncer(b *testing.B) {
-	b.Run("write file with buffer", func(b *testing.B) {
-		file, err := ioutil.TempFile("", "log")
-		require.NoError(b, err)
+// func BenchmarkBufferedWriteSyncer(b *testing.B) {
+// 	b.Run("write file with buffer", func(b *testing.B) {
+// 		file, err := ioutil.TempFile("", "log")
+// 		require.NoError(b, err)
 
-		defer func() {
-			assert.NoError(b, file.Close())
-			assert.NoError(b, os.Remove(file.Name()))
-		}()
+// 		defer func() {
+// 			assert.NoError(b, file.Close())
+// 			assert.NoError(b, os.Remove(file.Name()))
+// 		}()
 
-		w := &BufferedWriteSyncer{
-			WS: AddSync(file),
-		}
-		defer w.Stop()
-		b.ResetTimer()
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				w.Write([]byte("foobarbazbabble"))
-			}
-		})
-	})
-}
+// 		w := &BufferedWriteSyncer{
+// 			WS: AddSync(file),
+// 		}
+// 		defer w.Stop()
+// 		b.ResetTimer()
+// 		b.RunParallel(func(pb *testing.PB) {
+// 			for pb.Next() {
+// 				w.Write([]byte("foobarbazbabble"))
+// 			}
+// 		})
+// 	})
+// }
